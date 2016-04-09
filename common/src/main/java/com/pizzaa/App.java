82175e91151aebf76423750400a165f85b729736
Pawel Jankowski <jjpikoov@gmail.com>
2016-04-09 12:44:07 +0200

    Project split.

---
+++ common/src/main/java/com/pizzaa/App.java
@@ -0,0 +1,13 @@
+package com.pizzaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
